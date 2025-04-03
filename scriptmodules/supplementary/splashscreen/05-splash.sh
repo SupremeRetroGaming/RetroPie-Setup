771aa4a3c3f23bd5efc661e955633eace656bc44
SupremeRetroGaming <152055598+SupremeRetroGaming@users.noreply.github.com>
2025-04-02 23:22:40 -0400

    Delete scriptmodules/supplementary/splashscreen directory

--- scriptmodules/supplementary/splashscreen/05-splash.sh
+++
@@ -1,8 +0,0 @@
-PID_FILE=/dev/shm/rp-splashscreen.pid
-if [ "`tty`" = "/dev/tty1" ] && [ -z "$DISPLAY" ] && [ -f "$PID_FILE" ]; then
-    PID=`cat $PID_FILE`
-    if ps -p $PID >/dev/null; then
-        kill $PID >/dev/null 2>&1
-    fi
-    rm $PID_FILE
-fi
